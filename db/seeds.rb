3642f8f8b90b14c3fe38a6ebf3abc8fcb322f44d
viazb <victor.diaz.bascurgmail.com>
2017-10-20 19:14:04 -0300

    scafold add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pin.destroy_all
+p = [600,700,800,1000,1200,1400]
+
+20.times do |i|
+    Pin.create(titile:"Pin número #{i+1}", image: "http://lorempixel.com/#{p.sample}/#{p.sample}")
+end
