1a8826dc7a9469fa5f9c2fbbbdc3e048e56e7720
viazb <victor.diaz.bascurgmail.com>
2017-10-20 21:12:02 -0300

    agrego devise modelo User, modificacion de vistas de devise y navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :pins
+  devise_for :users
+  resources :pins #genera las rutas rest de los pins
   root 'pins#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
