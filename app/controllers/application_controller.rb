4d9e1a7a71d228a2c70bcaaf38d3432c35de89c0
viazb <victor.diaz.bascurgmail.com>
2017-11-04 11:34:01 -0300

    arreglo error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  check_authorization unless: :devise_controller #no permite el ingreso a un usuario a un metodo que no este utilizando authorize, a menos que se este trabajando en un controlador de devise.
+  check_authorization unless: :devise_controller? #no permite el ingreso a un usuario a un metodo que no este utilizando authorize, a menos que se este trabajando en un controlador de devise.
 
   rescue_from CanCan::AccessDenied do |exception|
           respond_to do |format|
